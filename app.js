a8b2b75603b93662cb206d71d14e1329858aae9f
Avadhesh <avadhesh.1001@gmail.com>
2016-08-18 12:25:59 +0000

    Initial Project file structure and bare min node.js express app

---
+++ app.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+//Express App created
+var app = express();
+
+app.onAppStart = function(addr) {
+  console.log("Samarth-SkillProfile web app is now Running on port:",addr.port);
+}
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'bower_modules')));
+app.use(express.static(path.join(__dirname, 'webapp')));
+
+app.use(function(req, res, next) {
+  var err = new Error('Resource not found');
+  err.status = 404;
+  return res.status(err.status).json({
+    "error": err.message
+  });
+});
+
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    logger.error("Internal error in watch processor: ", err);
+    return res.status(err.status || 500).json({
+      "error": err.message
+    });
+  });
+}
+
+app.use(function(err, req, res, next) {
+  logger.error("Internal error in watch processor: ", err);
+  return res.status(err.status || 500).json({
+    "error": err.message
+  });
+});
+
+module.exports = app;
