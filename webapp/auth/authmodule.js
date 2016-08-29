16003742a08607262281d74ec1fc11ac67384901
unknown <afrien08@gmail.com>
2016-08-29 15:55:08 +0530

    Login

--- webapp/auth/authmodule.js
+++ webapp/auth/authmodule.js
@@ -1,7 +1,9 @@
 angular.module('sm-skillprofile')
     .config(['$stateProvider', '$urlMatcherFactoryProvider', '$urlRouterProvider',
         function($stateProvider, $urlMatcherFactoryProvider, $urlRouterProvider) {
+
             $urlMatcherFactoryProvider.caseInsensitive(true);
+
             $stateProvider
                 .state('skillprofile.signin', {
                     url: '/signin',
--- webapp/smskillprofileapp.js
+++ webapp/smskillprofileapp.js
@@ -1,16 +1,18 @@
-var myApp = angular.module("sm-skillprofile", ['ngMaterial',
-        'ngAnimate',
-        'ngMessages',
-        'ui.router',
-        'ui.bootstrap'
-    ])
-    .config(['$urlMatcherFactoryProvider',
-        function($urlMatcherFactoryProvider) {
-            $urlMatcherFactoryProvider.caseInsensitive(true);
-        }
-    ])
-    .controller('sm-skillprofilectrl', ['$scope',
-        function($scope) {
-            //TBD
-        }
-    ]);
+var app = angular.module("sm-skillprofile", ['ngMaterial',
+    'ngAnimate',
+    'ngMessages',
+    'ui.router',
+    'ui.bootstrap'
+]);
+
+app.config(['$urlMatcherFactoryProvider',
+    function($urlMatcherFactoryProvider) {
+        $urlMatcherFactoryProvider.caseInsensitive(true);
+    }
+]);
+
+app.controller('skillprofilectrl', ['$scope',
+    function($scope) {
+        $scope.app = "Samarth-SkillProfile";
+    }
+]);
