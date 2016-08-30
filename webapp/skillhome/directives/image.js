8ffb0d38486da3b350b4fca1dd49854a90126cae
firedemon1 <codingdeepak@gmail.com>
2016-08-30 20:42:09 +0530

    skill layout

---
+++ webapp/skillhome/directives/image.js
@@ -0,0 +1,10 @@
+angular.module("sm-skillprofile")
+.directive('backImg', function(){
+    return function(scope, element, attrs){
+        var url = attrs.backImg;
+        element.css({
+            'background-image': 'url(' + url +')'
+           
+        });
+    };
+});​
