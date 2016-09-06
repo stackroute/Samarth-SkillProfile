1fa4103ec2e631103a5d7b36ee36083f3953f45c
Afrien <afrien08@gmail.com>
2016-09-06 04:07:23 +0000

    personalinfo

---
+++ webapp/notifications/sectionpersonalinfocard.js
@@ -0,0 +1,13 @@
+angular.module('MyApp')
+  .controller('AppCtrl', function($scope) {
+   
+  $scope.name='afrin';
+  $scope.age=24;
+  $scope.dob='05/06/1992';
+  $scope.matstatus='unmarried';
+  $scope.contact=9898765434;
+  $scope.email='afrien@gmail.com';
+  $scope.address='7th block koramangala ,bangalore,karnataka';
+  $scope.pin=564782;
+  
+  });
