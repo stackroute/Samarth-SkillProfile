3e0781db4ddc126c3bb7c6141001d0d60e5e110d
Avadhesh <avadhesh.1001@gmail.com>
2016-09-22 10:16:20 +0000

    qBox service modified

--- webapp/webcomponents/questionbox/services/questionboxService.js
+++ webapp/webcomponents/questionbox/services/questionboxService.js
@@ -1,39 +1,18 @@
 angular.module('sm-skillprofile')
-    .service('quesnboxService', function($http) {
+    .service('quesnboxService', function($http, $timeout) {
         return {
             questionGenerator: function() {
-                questionArray = ["What's your College Name?", "Where do you live?",
-                    "Where do you work?", "What's your birth place", "What's your favourite book"
-                ];
+                questionArray = [];
 
                 $http({
                     method: 'GET',
-                    url: 'http://localhost:8081/profile/101'
+                    url: 'http://localhost:8081/candidates/101/qboxquestions?sections=project&limit=2&skip=0'
                 }).then(function successCallback(response) {
+                    for (var key = 0; key < response.data.length; key++) {
+                        var section = response.data[key].section;
+                        var fieldname = response.data[key].fieldname;
+                        console.log("Inside qBox service success response", section + " ", fieldname);
 
-                    for (var noOfObjects = 0; noOfObjects < response.data.length; noOfObjects++) {
-                        for (var record = 0; record < response.data[noOfObjects].projects.length; record++) {
-                            for (var prop in response.data[0].projects[record]) {
-                                if (response.data[noOfObjects].projects[record][prop] == "" && prop != "meta") {
-                                    var temp = "What's your project " + prop
-                                    questionArray.push(temp);
-                                    console.log("From If", prop);
-                                }
-                            }
-                        }
-
-                        for (var record = 0; record < response.data[noOfObjects].skills.length; record++) {
-                            for (var prop in response.data[0].skills[record]) {
-                                var skill;
-                                if (prop=="skillname") {
-                                    skill = response.data[noOfObjects].skills[record][prop];
-                                }
-                                if (response.data[noOfObjects].skills[record][prop] == "" && prop != "metadata") {
-                                    var temp = "What's your " + prop + " of " + skill + " skill"
-                                    questionArray.push(temp);
-                                }
-                            }
-                        }
                     }
                 }, function errorCallback(response) {
                     console.log('Error accord during Project Section')
